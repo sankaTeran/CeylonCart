2876b1781c1da05ebd9ca4e8b2d94e78f10a15c9
unknown <sankagunarathna3@gmail.com>
2026-05-21 13:33:53 +0530

    frontend vercel url updated

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ await connectCloudinary();
 
 
 // Allow multiple origins
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173','https://ceylon-cart-frontend.vercel.app']
 
 app.post('/stripe',express.raw({type:'application/json'}),stripeWebhooks)
 
